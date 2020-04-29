0a1271d6b74085f0e0ae7858fd7f3618d781ba65
Kyle Platt <kyleplatt2014@gmail.com>
2020-04-29 19:04:22 -0400

    hotfix - adding switch to router

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './app.css';
 import CurrentForecast from '../CurrentForecast/currentForecast';
 import FiveDayForecast from '../FiveDayForecast/fiveDayForecast';
@@ -8,9 +8,11 @@ import * as ROUTES from '../../constants/routes';
 
 const App = () => (
     <Router>
-        <Route exact path={ROUTES.CURRENT_FORECAST} component={CurrentForecast} />
-        <Route path={ROUTES.FIVE_DAY_FORECAST} component={FiveDayForecast} />
-        <Route component={NotFound} />
+        <Switch>
+            <Route exact path={ROUTES.CURRENT_FORECAST} component={CurrentForecast} />
+            <Route path={ROUTES.FIVE_DAY_FORECAST} component={FiveDayForecast} />
+            <Route component={NotFound} />
+        </Switch>
     </Router>
 );
 
